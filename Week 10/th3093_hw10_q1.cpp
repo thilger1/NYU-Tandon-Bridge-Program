9a81f5683eb82103408e2034979d0b07fc3244c9
thilger1 <79420353+thilger1@users.noreply.github.com>
2023-03-12 22:53:20 -0400

    add boilerplate functions

--- Week 10/th3093_hw10_q1.cpp
+++ Week 10/th3093_hw10_q1.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+using namespace std;
 
+string* createWordsArray(string sentence, int& outWordsArrSize) {
+    string words[];
+    return words;
+}
+
+int main() {
+    //createWordsArray();
+    return 0;
+}
