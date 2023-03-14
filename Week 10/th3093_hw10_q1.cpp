ef5986bd9740ecdeafd58399541e35adff22ae63
thilger1 <79420353+thilger1@users.noreply.github.com>
2023-03-13 22:29:48 -0400

    testing push

--- Week 10/th3093_hw10_q1.cpp
+++ Week 10/th3093_hw10_q1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string.h>
 using namespace std;
 
 string* createWordsArray(string sentence, int& outWordsArrSize) {
@@ -7,6 +8,16 @@ string* createWordsArray(string sentence, int& outWordsArrSize) {
 }
 
 int main() {
-    //createWordsArray();
+    string userSentence;
+    cout<<"Please enter a sentence"<<endl;
+    getline(cin, userSentence);
+    int words = 0;
+    if (userSentence.length() > 0)
+        words += 1;
+    for (int i = 0; i < userSentence.length(); i++) {
+        if (userSentence[i] == " ")
+            words++;
+    }
+    createWordsArray(userSentence, &words);
     return 0;
 }
