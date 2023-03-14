36221d89f0316d43e4bc479fd7740663f3e4511a
thilger1 <79420353+thilger1@users.noreply.github.com>
2023-03-13 22:40:41 -0400

    mostly populated main

--- Week 10/th3093_hw10_q1.cpp
+++ Week 10/th3093_hw10_q1.cpp
@@ -3,21 +3,16 @@
 using namespace std;
 
 string* createWordsArray(string sentence, int& outWordsArrSize) {
-    string words[];
-    return words;
+    string* arr[outWordsArrSize];
+    return arr;
 }
 
 int main() {
     string userSentence;
     cout<<"Please enter a sentence"<<endl;
     getline(cin, userSentence);
-    int words = 0;
-    if (userSentence.length() > 0)
-        words += 1;
-    for (int i = 0; i < userSentence.length(); i++) {
-        if (userSentence[i] == " ")
-            words++;
-    }
-    createWordsArray(userSentence, &words);
+    int words;
+
+    //string* wordsArray = createWordsArray(userSentence, &words);
     return 0;
 }
