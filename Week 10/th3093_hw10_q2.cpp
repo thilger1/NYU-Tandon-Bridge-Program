b362faffe6007bde2b4c159c991b11315c8cec92
thilger1 <79420353+thilger1@users.noreply.github.com>
2023-03-15 08:23:02 -0400

    create variables and call findMissing in main

--- Week 10/th3093_hw10_q2.cpp
+++ Week 10/th3093_hw10_q2.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std;
 
+//int* findMissing(int arr[], int n, int& resArrSize) {
+
+//}
+
 int main() {
+    int arr[6] = {3, 1, 3, 0, 6, 4};
+    int resArrSize = 0;
+    int newArr[] = findMissing(arr, arr.length(), resArrSize);
     return 0;
 }
