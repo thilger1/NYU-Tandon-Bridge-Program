93361f9fd1aa95caf7fd360800971c1948d779b4
thilger1 <79420353+thilger1@users.noreply.github.com>
2023-03-15 07:58:03 -0400

    functional, add brackets and quotes to words on output

--- Week 10/th3093_hw10_q1.cpp
+++ Week 10/th3093_hw10_q1.cpp
@@ -3,19 +3,21 @@
 using namespace std;
 
 string* createWordsArray(string sentence, int& outWordsArrSize) {
-    int spaceIndex = 0;
+    //setting first spaceIndex to -1 to start first word before the sentence starts
+    //subsequent words will start after spaces
+    int spaceIndex = -1;
     int words = 0;
     int sentenceLength = sentence.length();
     string* wordsArray = new string[sentenceLength];
 
     for (int i = 0; i < sentenceLength; i++) {
         if (sentence[i] == ' ' || sentence[i] == '.') {
-            wordsArray[words] = sentence.substr(spaceIndex + 1, i - spaceIndex);
+            wordsArray[words] = sentence.substr(spaceIndex + 1, i - spaceIndex - 1);
             words++;
             spaceIndex = i;
         }
-        //if no spaces or periods in the sentece
-        
+    
+    //if no spaces or periods in the sentence
     }
     wordsArray[words] = sentence.substr(spaceIndex + 1);
     words++;
@@ -29,12 +31,14 @@ int main() {
     getline(cin, userSentence);
     int words;
 
-    string* wordsArray = createWordsArray(userSentence, &words);
-    cout<<"Words: ";
+    string* wordsArray = createWordsArray(userSentence, words);
+    cout<<"[";
     for (int i = 0; i < words; i++) { 
-        cout<<wordsArray[i];
+        cout<<"\""<<wordsArray[i]<<"\"";
         if (i != words-1)
             cout<<", ";
+        else
+            cout<<"]";
     }
     cout<<endl;
     delete[] wordsArray;
