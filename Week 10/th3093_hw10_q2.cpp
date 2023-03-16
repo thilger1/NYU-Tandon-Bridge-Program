86ca81a3caac49288cdcc6293166e9177be3d537
thilger1 <79420353+thilger1@users.noreply.github.com>
2023-03-15 22:58:23 -0400

    create findmissing pointer arrays

--- Week 10/th3093_hw10_q2.cpp
+++ Week 10/th3093_hw10_q2.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
 using namespace std;
 
-//int* findMissing(int arr[], int n, int& resArrSize) {
-
-//}
+int* findMissing(int arr[], int n, int& resArrSize) {
+    int* missing = new int[n + 1];
+    for (int i = 0; i <= n; i++)
+        missing[i] = 0;
+    for (int i = 1; i < n; i++)
+        missing[arr[i] - 1] = 1;
+    for (int i = 0; i < n; i++){}
+    int* existing = arr[highest-lowest];
+    int count = 0;
+    for (int i = lowest; i < highest; i++) {
+        if ()
+    }
+}
 
 int main() {
     int arr[6] = {3, 1, 3, 0, 6, 4};
     int resArrSize = 0;
-    int newArr[] = findMissing(arr, arr.length(), resArrSize);
+    int* newArr = findMissing(arr, arr.length(), resArrSize);
+    delete[] newArr;
+    newArr = nullptr;
     return 0;
 }
