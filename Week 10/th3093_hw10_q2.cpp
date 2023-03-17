cb8700e4046ce87b50fb84affe1f1b3378e0b852
thilger1 <79420353+thilger1@users.noreply.github.com>
2023-03-17 16:27:55 -0400

    functional q2

--- Week 10/th3093_hw10_q2.cpp
+++ Week 10/th3093_hw10_q2.cpp
@@ -2,23 +2,39 @@
 using namespace std;
 
 int* findMissing(int arr[], int n, int& resArrSize) {
-    int* missing = new int[n + 1];
-    for (int i = 0; i <= n; i++)
-        missing[i] = 0;
-    for (int i = 1; i < n; i++)
-        missing[arr[i] - 1] = 1;
-    for (int i = 0; i < n; i++){}
-    int* existing = arr[highest-lowest];
-    int count = 0;
-    for (int i = lowest; i < highest; i++) {
-        if ()
+
+    int* temp = new int[n + 1];
+    for (int i = 0; i < n; i++){
+        temp[i] = 0;
     }
+    for (int i = 0; i < n; i++){
+        temp[arr[i]]++;
+    }
+    int* missing = new int[n];
+    int count = 0;
+    for (int i = 0; i <= n; i++) {
+        if (temp[i] == 0) {
+            missing[count] = i;
+            count++;
+        }
+    }
+    resArrSize = count;
+    delete[] temp;
+    temp = nullptr;
+    return missing;
 }
 
 int main() {
     int arr[6] = {3, 1, 3, 0, 6, 4};
-    int resArrSize = 0;
-    int* newArr = findMissing(arr, arr.length(), resArrSize);
+    int resArrSize;
+    int* newArr = findMissing(arr, 6, resArrSize);
+    cout<<"[";
+    for (int i = 0; i < resArrSize; i++) {
+        cout<<newArr[i];
+        if (i != resArrSize - 1)
+            cout<<", ";
+    }
+    cout<<"]";
     delete[] newArr;
     newArr = nullptr;
     return 0;
