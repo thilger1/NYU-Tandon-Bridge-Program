59fdc6cc1be389fd2787bf47e1683ce6c0f77d9f
thilger1 <79420353+thilger1@users.noreply.github.com>
2023-03-20 23:13:20 -0400

    added ptr for fun

--- Week 11/vector.cpp
+++ Week 11/vector.cpp
@@ -11,10 +11,14 @@ void fun (vector<int>& blah) {
 int main() {
     vector<int> numbers;
     fun(numbers);
-
+    int x = 3;
+    int *ptr = nullptr;
+    ptr = &x;
+    *ptr += 1;
     for (int i =0; i < 10; i++) {
         cout<<numbers[i] << endl;
     }
+    cout<<"x: " + x<<endl;
 
     return 0;
 }
