e8344dea64ab2337641a6e2727e748758c3a902d
thilger1 <79420353+thilger1@users.noreply.github.com>
2023-03-20 23:11:18 -0400

    add fun func

--- Week 11/vector.cpp
+++ Week 11/vector.cpp
@@ -2,14 +2,18 @@
 #include <vector>
 using namespace std;
 
+void fun (vector<int>& blah) {
+    for (int i = 1; i < 10; i++) {
+        blah.push_back(i);
+    }
+}
+
 int main() {
     vector<int> numbers;
-    for (int i = 1; i <= 10; i++) {
-        numbers.push_back(i);
-    }
+    fun(numbers);
 
-    for (int j = 0; j < 10; j++) {
-        cout<<numbers[i] <<endl;
+    for (int i =0; i < 10; i++) {
+        cout<<numbers[i] << endl;
     }
 
     return 0;
