3691f27cb33f7cbabeb4af9a9ff71b3e3e85af30
thilger1 <79420353+thilger1@users.noreply.github.com>
2023-03-21 23:23:46 -0400

    wrote new problem

--- Week 11/vector.cpp
+++ Week 11/vector.cpp
@@ -2,15 +2,9 @@
 #include <vector>
 using namespace std;
 
-int f(int* n, int m) {
-    *n = 10;
-    m = 10;
-    return *n + m;
-}
+//Given an array of numbers, write a function to move all 0s to the end of the array while maintaining the relative order of the non-zero elements. Do this in-place. Should run O(n)
+//Example: [0, 2, 0, 1, 0] -> [2, 1, 0, 0, 0]
 
-int main() {
-    int n = 5;
-    int m = 5;
-    int res = f(&n, m);
-    cout<<res + n + m <<endl;
+void moveZeroes(int nums[], int n) {
+    
 }
