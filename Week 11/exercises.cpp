41039d48cb497dc6d62fca37914aae3a595d5e5c
thilger1 <79420353+thilger1@users.noreply.github.com>
2023-03-22 18:31:24 -0400

    redo functions

--- Week 11/exercises.cpp
+++ Week 11/exercises.cpp
@@ -6,22 +6,21 @@ using namespace std;
 //start at end of array
 
 bool isSorted(int* S, int length){
-    //base case
-    if (length == 1 || length == 0) {
+    if (length == 0 || length == 1) {
         return true;
     }
-    //recursive case check if the first element is greather than or equal to second element
     if (S[0] >= S[1]) {
-        return isSorted(S+1, length-1);
-
+        return isSorted(S+1, length - 1);
     }
+    
     return false;
+    
 }
 
-const int LENGTH = 8;
+const int LENGTH = 5;
 int main() {
-    int arr[LENGTH] = {100, 75, 27, 15, 8, -5, -5, -10};
-    cout<< boolalpha << isSorted(arr, LENGTH);
 
+    int arr[LENGTH] = {-3, 2, 6, 7, 8};
+    cout<< boolalpha << isSorted(arr, LENGTH);
     return 0;
 }
