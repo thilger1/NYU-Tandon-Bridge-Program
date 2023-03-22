d1419d5bb24e13aaf99867bccc434bb25fc11e5c
thilger1 <79420353+thilger1@users.noreply.github.com>
2023-03-22 18:48:40 -0400

    rewrote fib

--- Week 11/exercises.cpp
+++ Week 11/exercises.cpp
@@ -1,24 +1,11 @@
 #include <iostream>
 using namespace std;
 
-//check to see if an array is sorted or not recursively
-//base case: 1 element (or 8)
-//start at end of array
+//recursive function to check if it's a double palindrome
 
-bool isSorted(int* S, int length) {
-    //base case
-    if (length == 0 || length == 1) {
-        return true;
+int fib(int n) {
+    if (n <= 1){
+        return n;
     }
-    if (S[0] >= S[1]) {
-        return isSorted(S+1, length-1);
-    }
-    return false;
-}
-
-const int LENGTH = 5;
-int main() {
-    int arr[LENGTH] = {5, 4, 3, 2, 1};
-    cout<< boolalpha << isSorted(arr, LENGTH);
-    return 0;
+    return fib(n-1) + fib(n-2);
 }
