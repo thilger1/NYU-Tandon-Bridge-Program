e234a2666a0cc4675aadb4d0bc8563ca3cdb2e81
thilger1 <79420353+thilger1@users.noreply.github.com>
2023-03-21 23:41:36 -0400

    add note about in place

--- Week 11/vector.cpp
+++ Week 11/vector.cpp
@@ -5,6 +5,8 @@ using namespace std;
 //Given an array of numbers, write a function to move all 0s to the end of the array while maintaining the relative order of the non-zero elements. Do this in-place. Should run O(n)
 //Example: [0, 2, 0, 1, 0] -> [2, 1, 0, 0, 0]
 
+//making changes in place means that you cannot create a new array, need to redo
+
 void moveZeroes(int nums[], int n) {
     int zeroCount = 0;
     int count = 0;
