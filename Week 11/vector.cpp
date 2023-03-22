3a90d4990589d448ebf131b1ea79c278395805db
thilger1 <79420353+thilger1@users.noreply.github.com>
2023-03-21 22:45:39 -0400

    change exercise

--- Week 11/vector.cpp
+++ Week 11/vector.cpp
@@ -2,23 +2,8 @@
 #include <vector>
 using namespace std;
 
-void fun (vector<int>& blah) {
-    for (int i = 1; i < 10; i++) {
-        blah.push_back(i);
-    }
-}
-
-int main() {
-    vector<int> numbers;
-    fun(numbers);
-    int x = 3;
-    int *ptr = nullptr;
-    ptr = &x;
-    *ptr += 1;
-    for (int i =0; i < 10; i++) {
-        cout<<numbers[i] << endl;
-    }
-    cout<<"x: " + x<<endl;
-
-    return 0;
+int f(int* n, int m) {
+    *n = 10;
+    m = 10;
+    return *n + m;
 }
