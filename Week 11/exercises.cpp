cbb3411f5d6e3c2347ccf590e32f60a68d25c9e1
thilger1 <79420353+thilger1@users.noreply.github.com>
2023-03-22 18:34:54 -0400

    switching order of array

--- Week 11/exercises.cpp
+++ Week 11/exercises.cpp
@@ -5,22 +5,20 @@ using namespace std;
 //base case: 1 element (or 8)
 //start at end of array
 
-bool isSorted(int* S, int length){
+bool isSorted(int* S, int length) {
+    //base case
     if (length == 0 || length == 1) {
         return true;
     }
     if (S[0] >= S[1]) {
-        return isSorted(S+1, length - 1);
+        return isSorted(S+1, length-1);
     }
-    
     return false;
-    
 }
 
 const int LENGTH = 5;
 int main() {
-
-    int arr[LENGTH] = {-3, 2, 6, 7, 8};
+    int arr[LENGTH] = {5, 4, 3, 2, 1};
     cout<< boolalpha << isSorted(arr, LENGTH);
     return 0;
 }
