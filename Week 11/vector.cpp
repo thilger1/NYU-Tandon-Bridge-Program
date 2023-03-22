65d85d8bd5350f059fcc39481d8e9e51c8a7fc87
thilger1 <79420353+thilger1@users.noreply.github.com>
2023-03-21 23:50:19 -0400

    back to passing pointers

--- Week 11/vector.cpp
+++ Week 11/vector.cpp
@@ -2,36 +2,16 @@
 #include <vector>
 using namespace std;
 
-//Given an array of numbers, write a function to move all 0s to the end of the array while maintaining the relative order of the non-zero elements. Do this in-place. Should run O(n)
-//Example: [0, 2, 0, 1, 0] -> [2, 1, 0, 0, 0]
+//back to passing pointers
 
-//making changes in place means that you cannot create a new array, need to redo
-
-void moveZeroes(int nums[], int n) {
-    int zeroCount = 0;
-    int count = 0;
-    int newNums[n];
-    for (int i = 0; i < n; i++) {
-        if (nums[i] == 0) {
-            zeroCount++;
-        }
-        else { 
-            newNums[count] = nums[i];
-            count++;
-        }
-    }
-    for (int i = 0; i < zeroCount; i++) {
-        newNums[count + i] = 0;
-        }
-    for (int i = 0; i < n; i++) {
-        cout<<newNums[i]<<" ";
-        }
-    cout<<endl;
+void increasePrice(double price) {
+    price *= 1.2;
 }
 
 int main() {
-    int nums[5] = {0, 2, 0, 1, 0};
-    int length = 5;
-    moveZeroes(nums, length);
+    double price = 100;
+    increasePrice(price);
+    cout << price;
+
     return 0;
 }
