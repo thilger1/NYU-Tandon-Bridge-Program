264be92921ad2dd514a4d7fcb5ed9c9551f1e43d
thilger1 <79420353+thilger1@users.noreply.github.com>
2023-03-24 16:34:48 -0400

    add strings

--- Additional Exercises/stringList.cpp
+++ Additional Exercises/stringList.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <vector>
+#include <string>
 using namespace std;
 
+string analyzeString() {
+
+}
+
 int main() {
+    string userString;
+    while (userString != " "){
+        if (userString == " ") {
+            return;
+        }
+        else {
+            analyzeString()
+        }
+    }
     return 0;
 }
