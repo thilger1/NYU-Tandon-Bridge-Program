77e60ee46920b8697443c0a135018b1b11263258
thilger1 <79420353+thilger1@users.noreply.github.com>
2023-03-26 18:11:29 -0400

    base case

--- Additional Exercises/printAscending.cpp
+++ Additional Exercises/printAscending.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 using namespace std;
 
-void printAscending() {
-    
+void printAscending(int start, int end) {
+    //step 1: base case
+    if (start == end) {
+        cout<<start<<endl;
+    }
 }
 
 int main() {
 
+    printAscending(3, 6);
     return 0;
 }
