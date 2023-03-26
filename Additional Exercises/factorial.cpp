2fb95cc7da96cf876199131862c3e4def3c8a250
thilger1 <79420353+thilger1@users.noreply.github.com>
2023-03-26 18:52:24 -0400

    functional

--- Additional Exercises/factorial.cpp
+++ Additional Exercises/factorial.cpp
@@ -2,9 +2,18 @@
 using namespace std;
 
 int factorial(int n) {
-
+    int res;
+    if (n == 1) {
+        return 1;
+    }
+    else {
+        res = factorial(n-1);
+        res *= n;
+        return res;
+    }
 }
 
 int main() {
+    cout<<factorial(4)<<endl;
     return 0;
 }
