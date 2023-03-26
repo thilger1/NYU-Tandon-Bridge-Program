a6732351dd20172cbb933102c83636f625ba973a
thilger1 <79420353+thilger1@users.noreply.github.com>
2023-03-26 18:14:56 -0400

    recursive step

--- Additional Exercises/printAscending.cpp
+++ Additional Exercises/printAscending.cpp
@@ -5,6 +5,11 @@ void printAscending(int start, int end) {
     //step 1: base case
     if (start == end) {
         cout<<start<<endl;
+    //step 2: recursive step
+    //define the inductive hypothesis (if we call print ascending, it will print the numbers in that range)
+    else {
+        printAsC(start, end-1);
+    }
     }
 }
 
