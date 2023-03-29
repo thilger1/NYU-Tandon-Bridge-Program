6cf1027d673e217246e244645a66dafcbeac2258
thilger1 <79420353+thilger1@users.noreply.github.com>
2023-03-28 23:01:14 -0400

    full employee class

--- Week 12/ooppractice.cpp
+++ Week 12/ooppractice.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+using std::string;
 
 class Employee {
     //semicolon belongs at the end of program
-    
+    string Name;
+    string Company;
+    int Age;
+
 };
 
 int main() {
-
+    int number;
 }
