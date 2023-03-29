7a03861e33dbbb228dfa735dd6e0275d83b8d497
thilger1 <79420353+thilger1@users.noreply.github.com>
2023-03-29 08:49:11 -0400

    copy constructor

--- Week 12/ooppractice.cpp
+++ Week 12/ooppractice.cpp
@@ -2,23 +2,19 @@
 #include <string.h>
 using namespace std;
 
-class Simple {
-public:
-    void display() {cout << "Displaying a Simple object\n";}
-};
-
 class Vorlon {
-public:
-    Vorlon(const string& aName) {myName = aName;}
+public: 
+    Vorlon(const string& aName) : myName(aName) {}
     void display() {cout << "Displaying a Vorlon named " << myName << endl;}
 private:
     string myName;
-};
+}
 //remember semicolon at the end or it wil get an error
 
 int main() {
     Vorlon v2("Kosh");
-    v2.display();
+    Vorlon v3(v1);
+    Vorlon v4 = v1;
     return 0;
 }
 
