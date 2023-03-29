86a25694c8a2d8727c6895d7ae69e95bcfac74bf
thilger1 <79420353+thilger1@users.noreply.github.com>
2023-03-29 08:31:24 -0400

    create private var

--- Week 12/ooppractice.cpp
+++ Week 12/ooppractice.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
+#include <string.h>
 using namespace std;
 
 class Simple {
 public:
     void display() {cout << "Displaying a Simple object\n";}
 };
+
+class Vorlon {
+public:
+    void display() {cout << "Displaying a Vorlon named " << myName << endl;}
+private:
+    string myName;
+};
 //remember semicolon at the end or it wil get an error
 
 int main() {
-    Simple simpleton;
-    simpleton.display();
+    Vorlon v1;
+    v1.myName = "Kosh";
+    v1.display();
     return 0;
 }
 
