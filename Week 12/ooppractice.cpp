b1162be309c84b292130bf4df4e6eaad23d011ec
thilger1 <79420353+thilger1@users.noreply.github.com>
2023-03-31 08:40:30 -0400

    privatize Car vars

--- Week 12/ooppractice.cpp
+++ Week 12/ooppractice.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 using namespace std;
 
+class Car {
+private:
+    double _height;
+    double _weight;
+    string _color;
+    int _numOfWheels;
+    int _year;
+
+    Car(double weight){
+        _weight = weight;
+    }
+
+};
+
 int main() {
+    Car chevy(15);
+    Car tesla(10);
+
+    cout << chevy.weight;
 
     return 0;
 }
