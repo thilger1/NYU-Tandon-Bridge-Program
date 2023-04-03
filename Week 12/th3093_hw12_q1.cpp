6dc19241f84c95f4c10d427ffe0f051843ae4d65
thilger1 <79420353+thilger1@users.noreply.github.com>
2023-04-03 08:35:57 -0400

    Money redefine

--- Week 12/th3093_hw12_q1.cpp
+++ Week 12/th3093_hw12_q1.cpp
@@ -3,45 +3,39 @@ using namespace std;
 
 class Money {
 public:
-    friend Money add(Money amount1, Money amount2);
-    //Precondition: amount1 and amount2 have been given values.
+    friend Money operator +(const Money& amount1, const Money& amount2);
     //Returns the sum of the values of amount1 and amount2.
-    friend bool equal(Money amount1, Money amount2);
-    //Precondition: amount1 and amount2 have been given values.
-    //Returns true if the amount1 and amount2 have the same value;
-    //otherwise, returns false.
+    friend Money operator -(const Money& amount1, const Money& amount2);
+    //Returns amount1 minus amount2.
+    friend Money operator -(const Money& amount);
+    //Returns the negative of the value of amount.
+    friend bool operator ==(const Money& amount1, const Money& amount2);
+    //Returns true if amount1 and amount2 have the same value; false otherwise.
+    friend bool operator <(const Money& amount1, const Money& amount2);
+    //Returns true if amount1 is less than amount2; false otherwise.
     Money(long dollars, int cents);
-    //Initializes the object so its value represents an amount with the
-    //dollars and cents given by the arguments. If the amount is negative,
-    //then both dollars and cents must be negative.
-    Money (long dollars);
+    //Initializes the object so its value represents an amount with
+    //the dollars and cents given by the arguments. If the amount
+    //is negative, then both dollars and cents should be negative.
+    Money(long dollars);
     //Initializes the object so its value represents $dollars.00.
-    Money();
+    Money( );
     //Initializes the object so its value represents $0.00.
-    double get_value();
-    //Precondition: The calling object has been given a value.
-    //Returns the amount of money recorded in the data of the calling object
-    void input(istream& ins);
+    double get_value( ) const;
+    //Returns the amount of money recorded in the data portion of the calling
+    //object.
+    friend istream& operator >>(istream& ins, Money& amount);
+    //Overloads the >> operator so it can be used to input values of type
+    //Money. Notation for inputting negative amounts is as in − $100.00.
     //Precondition: If ins is a file input stream, then ins has already been
-    //connected to a file. An amount of money, including a dollar sign, has been
-    //entered in the input stream ins. Notation for negative amounts is -$100.00.
-    //Postcondition: The value of the calling object has been set to
-    //the amount of money read from the input stream ins.
-    void output(ostream& outs);
+    //connected to a file.
+    friend ostream& operator <<(ostream& outs, const Money& amount);
+    //Overloads the << operator so it can be used to output values of type
+    //Money. Precedes each output value of type Money with a dollar sign.
     //Precondition: If outs is a file output stream, then outs has already been
     //connected to a file.
-    //Postcondition: A dollar sign and the amount of money recorded
-   //in the calling object have been sent to the output stream outs.
 private:
-    long all_cents;
-};
-
-class Check {
-private:
-    int check_number;
-    double amount;
-    bool is_cashed;
-
+    long all_cents
 };
 
 int main() {
