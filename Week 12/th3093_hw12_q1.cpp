35193ec9afa2dd5da1bda222a874d628d4adfaf5
thilger1 <79420353+thilger1@users.noreply.github.com>
2023-04-03 08:16:09 -0400

    add money class from textbook

--- Week 12/th3093_hw12_q1.cpp
+++ Week 12/th3093_hw12_q1.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 using namespace std;
 
+class Money {
+public:
+    friend Money add(Money amount1, Money amount2);
+    friend bool equal(Money amount1, Money amount2);
+    Money(long dollars, int cents);
+    Money (long dollars);
+    Money();
+    double get_value();
+    void input(istream& ins);
+    void output(ostream& outs);
+private:
+    long all_cents;
+};
+
 class Check {
 private:
     int check_number;
