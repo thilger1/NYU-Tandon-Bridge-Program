c45a8676fd3a1f253bc883096ee7f490795bb64f
thilger1 <79420353+thilger1@users.noreply.github.com>
2023-04-08 15:19:09 -0400

    Add boilerplate and psuedocode

---
+++ Week 13/th3093_hw13_q1.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+using namespace std;
+//20x20 grid, 5 dooglebugs, 100 ants
+
+
+//intialize grid
+    //20x20 2-dimensional array
+    //choose random spaces for 5 dbs
+    //choose random spaces for 100 ants
+    //intialize grid based on spaces for ants and dbs
+    
+    //5 doodlebugs
+        //Organism class
+            //doodlebug class
+    //100 ants
+        //Organism class
+            //ant class
+
+//Organism class
+    //movement
+    //
+
+//time step
+    //increment step #
+    //move ants & dbs
+    //check for eating
+    //check for breeding
+    //check for starvation
+int doodleBug() {
+
+    //initialize grid
+        //
+}
+
+
+int main() {
+    doodleBug();
+    return 0;
+}
