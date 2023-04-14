d83d7fd0db09d0d5ef05a6854cbe2db109493a86
thilger1 <79420353+thilger1@users.noreply.github.com>
2023-04-14 10:12:58 -0400

    Exercise 2, array largest

--- Week 13/practice.cpp
+++ Week 13/practice.cpp
@@ -2,16 +2,17 @@
 #include <string.h>
 using namespace std;
 
-void swap(int* a, int* b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+int findLargest(int* array, int n) {
+    int largest = array[0];
+    for (int i = 1; i < n; i++) {
+        if (array[i] > largest)
+            largest = array[i];
+    }
+    return largest;
 }
 
 int main() {
-    int x = 5, y = 10;
-    cout<<"Before swapping: x = " << x << ", y = " << y << endl;
-    swap(&x, &y);
-    cout<< "After swapping: x = " << x << ", y = " << y << endl;
+    int arr[5] = {3, 20, 5, 9, 1};
+    findLargest(arr, 5);
     return 0;
 }
