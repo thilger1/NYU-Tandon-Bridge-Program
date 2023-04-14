e74a458057c0d579b319ba167ce8c3acc02e4fb8
thilger1 <79420353+thilger1@users.noreply.github.com>
2023-04-13 20:49:03 -0400

    Shapes example

--- Week 13/practice.cpp
+++ Week 13/practice.cpp
@@ -2,36 +2,39 @@
 #include <string.h>
 using namespace std;
 
-class Animal {
+class Shape {
 public:
-    virtual void speak() {
-        cout<<"Animal speaks" <<endl;
+    void virtual getArea() {
+        cout<<"Area of shape: "<<endl;
     }
 };
 
-class Cat : public Animal {
+class Circle : public Shape {
+private:
+    int Radius;
 public:
-    void speak() {
-        cout<< "Meow" << endl;
+    Circle() : Radius(0) {};
+    Circle(int radius) : Radius(radius) {};
+    void getArea() {
+        cout<<"Area of circle: "<< (Radius * Radius) * 3.14 << endl;
     }
 };
 
-class Dog : public Animal {
+class Rectangle : public Shape {
+private:
+    int Height;
+    int Width;
 public:
-    void speak() {
-        cout<< "Woof" << endl;
+    Rectangle() : Height(0), Width(0) {};
+    Rectangle(int height, int width) : Height(height), Width(width) {};
+    void getArea() {
+        cout<<"Area of rectangle: "<< (Height * Width)<<endl;
     }
 };
-
-void speakTwice(Animal* animalPtr) {
-    animalPtr->speak();
-    animalPtr->speak();
-}
 
 int main() {
-    Cat cat;
-    Dog dog;
-    speakTwice(&cat);
-    speakTwice(&dog);
+    Shape* circlePtr = new Circle(5);
+    circlePtr->getArea();
+    delete circlePtr;
     return 0;
 }
