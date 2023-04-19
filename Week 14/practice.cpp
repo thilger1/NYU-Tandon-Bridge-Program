654b4d6499187f669d61283807d1e47fa2b2fe3f
thilger1 <79420353+thilger1@users.noreply.github.com>
2023-04-18 22:26:19 -0400

    sorted search algo 2

--- Week 14/practice.cpp
+++ Week 14/practice.cpp
@@ -1,11 +1,29 @@
-int search(int arr[], int arrSize, int val) {
-    int i;
+#include <iostream>
+using namespace std;
 
-    i = 0;
-    while (i < arrSize) {
-        if (arr[i] == val)
-            return i;
-        i++;
+int sortedSearch(int strArr[], int srtArrSize, int val);
+
+int main() {
+    int srtArr[10] = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19};
+    cout<<sortedSearch(srtArr, 10, 13)<<endl;
+    return 0;
+}
+
+int sortedSearch(int srtArr[], int srtArrSize, int val) {
+    int low, high, mid, ind;
+    bool found;
+    low = 0;
+    high = srtArrSize - 1;
+    found = false;
+    while (found == false) {
+        mid = (low + high) / 2;
+        if (srtArr[mid] == val) {
+            ind = mid;
+            found = true;
+        }
+        else if (val < srtArr[mid])
+            high = mid - 1;
+        else //val > srt[mid]
+            low = mid + 1;
     }
-    return -1;
 }
