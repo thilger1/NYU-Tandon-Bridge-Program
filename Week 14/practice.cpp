5843e6a8ec8988f34d3ae22f2efb3ec88add20d7
thilger1 <79420353+thilger1@users.noreply.github.com>
2023-04-19 18:42:40 -0400

    selection sort

--- Week 14/practice.cpp
+++ Week 14/practice.cpp
@@ -3,50 +3,15 @@
 #include <vector>
 using namespace std;
 
-int basicReadIn() {
-    ifstream file;
-    string filePath = "this is a filepath";
-    file.open(filePath);
-    char c;
-    vector <char> v;
-
-    while(file.get(&c)) {
-        v.push_back(c);
+void selectionSort(vector<int>& v) { //Runtime: Theta(N^2)
+    for (int i = 0; i < v.size(); i++) {
+        int minIndex = i;
+        for (int j = i + 1; j < v.size(); j++)
+            if (v[j] < v[minIndex])
+                    minIndex = j;
+            int temp = v[i];
+            v[i] = v[minIndex];
+            v[minIndex] = temp;
     }
-
-    file.close();
-    return 0;
 }
-
-int main() {
-    ifstream in_stream;
-    ofstream out_stream;
-
-    string inputFile = "input.txt";
-    in_stream.open(inputFile);
-    out_stream.open("output.txt");
-    string filePath = "this is a filepath";
-    in_stream.open(filePath);
-
-    if(in_stream) {
-        cout<<"Fail";
-        return 1;
-    }
-
-    char c;
-    vector<int> allNumbers;
-    string currLine;
-    int currNum;
-    while(getline(in_stream, currLine)) {
-        for (int i = 2; i < currLine.length(); i++) {
-            char c = currLine[i];
-            out_stream<< c;
-        }
-        out_stream << "\n";
-    }
-
-    return 0;
-}
-
-
-
+void insertionSort(vector)
