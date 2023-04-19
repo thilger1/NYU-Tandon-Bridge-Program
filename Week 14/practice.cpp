a535220a5469485c5bf8b7d014de870a1c7f85ea
thilger1 <79420353+thilger1@users.noreply.github.com>
2023-04-19 11:45:48 -0400

    selection sort

--- Week 14/practice.cpp
+++ Week 14/practice.cpp
@@ -1,34 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int sortedSearch(int strArr[], int srtArrSize, int val);
+int findIndexOfMin(int arr[], int low, int high) {
+    int min, minInd;
 
-int main() {
-    int srtArr[10] = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19};
-    cout<<sortedSearch(srtArr, 10, 13)<<endl;
-    return 0;
+    min = arr[low];
+    minInd = low;
+    for (int i = low+1; i <= high; i++){
+        if (arr[i] < min) {
+            min = arr[i];
+            minInd = i;
+        }
+    }
+    return minInd;
 }
 
-int sortedSearch(int srtArr[], int srtArrSize, int val) {
-    int low, high, mid, ind;
-    bool found;
-    low = 0;
-    high = srtArrSize - 1;
-    found = false;
-    while (found == false) {
-        mid = (low + high) / 2;
-        if (srtArr[mid] == val) {
-            ind = mid;
-            found = true;
-        }
-        else if (val < srtArr[mid])
-            high = mid - 1;
-        else //val > srt[mid]
-            low = mid + 1;
-    }
+void selectionSort(int arr[], int arrSize) {
+    int i, minInd;
 
-    if (found == true)
-        return ind;
-    else
-        return -1;
+    for (i = 0; i < arrSize; i++) {
+        minInd = findIndexOfMin(arr, i, arrSize-1);
+        swap(arr[i], arr[minInd]);
+    }
 }
