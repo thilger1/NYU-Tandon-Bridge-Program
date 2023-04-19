abe3dcf1e7c787245cb15f8c3cc9a41b0e78740e
thilger1 <79420353+thilger1@users.noreply.github.com>
2023-04-18 21:55:21 -0400

    add max and min

--- Week 14/th3093_hw14_q1.cpp
+++ Week 14/th3093_hw14_q1.cpp
@@ -2,7 +2,16 @@
 #include <vector>
 using namespace std;
 
+int divAndConq(vector <int> vec, int n, int &max, int &min) {
+    if (n == 1) {
+
+    }
+}
+
 int main() {
     vector <int> vec = {1, 2, 4, 5, 6, 7};
+    int n = vec.size();
+    int max, min;
+    divAndConq(vec, n, max, min);
     return 0;
 }
