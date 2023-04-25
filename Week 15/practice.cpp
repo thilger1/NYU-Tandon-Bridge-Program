065e3a14ee7da0c8b01dc99bb6ece5a4722434ec
thilger1 <79420353+thilger1@users.noreply.github.com>
2023-04-25 19:12:00 -0400

    practice read inFile

--- Week 15/practice.cpp
+++ Week 15/practice.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
+void openInputFile(ifstream& inFile){
+    string filename;
+    cout<<"What filename? ";
+    cin>>filename;
+    inFile.open(filename);
+    while (!inFile) {
+        cout<<"File failed to open"<<endl;
+        cout<<"What filename? ";
+        cin>>filename;
+        inFile.clear();
+        inFile.open(filename);
+    }
+}
+
 int main() {
-    ofstream outFile;
-    outFile.open("outfile.txt");
+    ifstream inFile;
+    openInputFile(inFile);
+    vector<int> v;
+    int temp;
+    while(inFile >> temp){
+        v.push_back(temp);
+    }
+
+    double sum = 0;
+    for (int item : v)
+        sum += item;
+    cout<<"The average of the integers in the file is: " << sum / v.size() <<endl;
 }
