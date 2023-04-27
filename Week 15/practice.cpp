24dbfe22c1c477372f070d73f333cab2159f0685
thilger1 <79420353+thilger1@users.noreply.github.com>
2023-04-26 21:30:44 -0400

    add node template

--- Week 15/practice.cpp
+++ Week 15/practice.cpp
@@ -4,40 +4,12 @@
 
 using namespace std;
 
-void openInputFile(ifstream& inFile){
-    string filename;
-    cout<<"What filename? ";
-    cin>>filename;
-    inFile.open(filename);
-    while (!inFile) {
-        cout<<"File failed to open"<<endl;
-        cout<<"What filename? ";
-        cin>>filename;
-        inFile.clear();
-        inFile.open(filename);
-    }
-}
-
-struct Student {
-    int id;
-    double testScore;
-    string name;
+template <class T>
+class LListNode {
+    T data;
+    LListNode<T>* next;
+public:
+    LListNode(T newdata = T(), LListNode<T>* newNext = nullptr) :
+        data(newdata), next(newNext) {}
+    friend class LList <T>;
 };
-
-int main() {
-    ifstream inFile;
-    openInputFile(inFile);
-    vector<Student> vs;
-    Student temp;
-    while(inFile >> temp.id){
-        inFile >> temp.testScore;
-        inFile.ignore(9999, '\t');
-        getline(inFile, temp.name);
-        vs.push_back(temp);
-    }
-    cout<<"Students with test scores over 90: "<<endl;
-    for (Student s : vs) {
-        if (s.testScore > 90)
-            cout<< s.name << endl;
-    }
-}
