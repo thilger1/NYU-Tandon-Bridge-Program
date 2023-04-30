8857ee392795cb47df5eb46ad764c8584c7841a1
thilger1 <79420353+thilger1@users.noreply.github.com>
2023-04-30 19:18:23 -0400

    queue enqueues

--- Week 16/practice.cpp
+++ Week 16/practice.cpp
@@ -1,16 +1,13 @@
 #include <iostream>
 #include <list>
+#include <queue>
+#include <stack>
 
 using namespace std;
 
-template<class T>
-class Stack{
-    list <T> data;
-public:
-    void push(T newItem) { data.push_front (newItem) ; }
-    T pop() { return data.pop_front() ;}
-    T top() const { return *data.begin();}
-    bool isEmpty() const{ return data.size() == 0;}
-    int size() const { return data.size();}
-    void clear() { data.clear(); }
-};
+int main() {
+    Queue<int> q;
+    q.enqueue(20);
+    q.enqueue(50);
+    q.enqueue(30);
+}
