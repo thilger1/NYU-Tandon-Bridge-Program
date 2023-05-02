f9fdc3155f35e7672ce3cd8645e17ed6da196888
thilger1 <79420353+thilger1@users.noreply.github.com>
2023-05-01 21:45:54 -0400

    revise member functions

--- Week 16/th3093_hw16_q1.cpp
+++ Week 16/th3093_hw16_q1.cpp
@@ -1,23 +1,48 @@
 #include <vector>
 #include <iostream>
+#include <string.h>
+
+//need to account for if vector is empty BEFORE calling pop
 
 using namespace std;
 
 class Stack {
 private:
     vector<char> vec;
-    int popTrack = 0;
+    char popped;
+    int size;
 public:
     void push(char newItem) { vec.push_back(newItem);}
-    char pop() { popTrack += 1; return vec[popTrack-1];}
-    char top() { return vec[popTrack]; }
+    char pop() { 
+        popped = vec[vec.size() - 1];
+        vec.pop_back();
+        return popped;
+    }
+    char top() { return vec[vec.size() - 1]; }
     int size() { return vec.size(); }
-    bool isEmpty { return vec.size() == 0;}
+    bool isEmpty() { return vec.size() == 0;}
     void clear() { vec.clear(); }
 
 };
 
 bool pascal(){
+    string begin;
+    string content;
+    string end;
+    char check;
+    Stack checker;
+    cout<<"Please enter your content."<<endl;
+    cin>>begin;
+    cin>>content;
+    cin>>end;
+    for (int i = 0; i < content.length(); i++) {
+        if (content[i] == '{' || content[i] == '(' || content[i] == '[') {
+            checker.push(content[i]);
+        }
+        if (content[i] == '}' || content[i] == ')' || content[i] == ']') {
+            check = Stack.
+        }
+    }
 
 }
 
