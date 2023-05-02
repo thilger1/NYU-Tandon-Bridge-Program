09a11458a0a8e2057e7c514333e76d9a4975dc48
thilger1 <79420353+thilger1@users.noreply.github.com>
2023-05-02 19:52:52 -0400

    practice read in file

--- Week 16/practice.cpp
+++ Week 16/practice.cpp
@@ -1,46 +1,28 @@
 #include <fstream>
-#include <list>
+#include <string>
+#include <iostream>
 
 using namespace std;
 
-template <class T>
-class Stack {
-    list<T> data;
-public:
-    void push(T newItem) {data.push_front(newItem); }
-    T pop() {return data.pop_front();}
-    T top()const{return *data.begin();}
-    bool isEmpty() const{return data.size() == 0;}
-    int size() const {return data.size(); }
-    void clear() { data.clear(); }
-};
+void readContent(ifstream& inFile) {
+    string line;
+    cout<<"start...";
+    inFile.open("content.txt");
+    if (!inFile)
+        cout<<"Failed to open";
+    if (getline(inFile, line)) {
+        string begin;
+        string content;
+        string end;
 
-
-#include <list>
-
-template <class T>
-class Stack {
-    list<T> data;
-public:
-    void push(T newItem) {data.push_front(newItem);}
-    T pop() {data.pop_front(); }
-    T top() const {return *data.begin();}
-    bool isEmpty() const {return data.size() == 0;}
-    int size() const{return data.size();}
-    void clear() {data.clear();}
-};
-
-
-#include <list>
-
-template <class T>
-class Stack {
-    list<T> data;
-public:
-    void push(T newItem) { return data.push_front(newItem);}
+        cin>>begin;
+        cin>>content;
+        cin>>end;
+        cout<<content<<endl;
+    }
 }
 
-template <class T>
-class Stack {
-    list<T> data;
+main() {
+    ifstream inFile;
+    readContent(inFile);
 }
