9e92633acc2c73ef5cdf11c7b4ed51ba4cce6c44
thilger1 <79420353+thilger1@users.noreply.github.com>
2023-05-01 20:17:45 -0400

    main func populated

--- Week 16/th3093_hw16_q1.cpp
+++ Week 16/th3093_hw16_q1.cpp
@@ -11,9 +11,20 @@ public:
     void push(char newItem) { vec.push_back(newItem);}
     char pop() { popTrack += 1; return vec[popTrack-1];}
     char top() { return vec[popTrack]; }
-    char 
+    int size() { return vec.size(); }
+    bool isEmpty { return vec.size() == 0;}
+    void clear() { vec.clear(); }
+
 };
 
+bool pascal(){
+
+}
+
 int main() {
+    if(pascal())
+        cout<<"True"<<endl;
+    else
+        cout<<"False"<<endl;
     return 0;
 }
