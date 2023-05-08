7f5a2779db88e5abc4db6f3a88899db9aa8da92e
thilger1 <79420353+thilger1@users.noreply.github.com>
2023-05-07 21:33:28 -0400

    getline practice

--- Week 16/practice.cpp
+++ Week 16/practice.cpp
@@ -1,13 +1,11 @@
-template <class T>
-class BSTNode {
-    T data;
-    BSTNode<T>* parent;
-    BSTNode<T>* left;
-    BSTNode<T>* right;
-public:
-    BSTNode(T newdata = T(), BSTNode<T>* newParent = nullptr, BSTNode<T>* newLeft = nullptr, BSTNode<T>* newRight = nullptr)
-    : data(newdata), parent(newParent), left(newLeft), right(newRight) {}
-    friend class BST<T>;
-    int getSize() const;
-};
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main() {
+    string line;
+    getline(cin, line);
+    cout<<"String: "<<line<<endl;
+    return 0;
+}
 
