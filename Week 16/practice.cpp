97c041d3351bb514b6bc86434b31fca873108876
thilger1 <79420353+thilger1@users.noreply.github.com>
2023-05-10 18:57:00 -0400

    add llistnode class

--- Week 16/practice.cpp
+++ Week 16/practice.cpp
@@ -3,6 +3,16 @@
 #include <fstream>
 using namespace std;
 
+template <class T>
+class LListNode{
+    T data;
+    LListNode<T>* next;
+public:
+    LListNode(T newdata = T(), LListNode<T>* newNext = nullptr) :
+        data(newdata), next(newNext) {}
+    friend class LList <T>;
+};
+
 
 ListNode* reverse_linked_list(ListNode* head) {
     ListNode* prev = nullptr;
