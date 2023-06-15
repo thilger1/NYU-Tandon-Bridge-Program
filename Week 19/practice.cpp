bd93f5b188386c24cb7ae6c755a66533f3105496
thilger1 <79420353+thilger1@users.noreply.github.com>
2023-06-14 22:41:22 -0400

    stack code

--- Week 19/practice.cpp
+++ Week 19/practice.cpp
@@ -1,26 +1,18 @@
-#include <stdio.h>
-#include <vector>
+//stack code
+
 #include <iostream>
+#include <list>
 
 using namespace std;
 
+template<class T>
 class Stack {
-    vector<char> vec;
-    char popped;
+    list<T> data;
 public:
-    void push(char newItem) {vec.push_back(newItem);}
-    char pop() {
-        popped = vec[vec.size() - 1];
-        vec.pop_back();
-        return popped;
-    }
-    char top() { return vec[vec.size() - 1]; }
-    int size() 
-
-
-    inFile.open("content.txt");
-    for (int i = 0; i < content.length(); i++) {
-        if (content[i] == '{')
-    }
-    inFile.close();
-}
+    void push(T newItem) { data.push_front(newItem);}
+    T pop() { return data.pop_front();}
+    T top() const { return *data.begin();}
+    bool isEmpty() const{ return data.size() == 0;}
+    int size() const { return data.size();}
+    void clear() { data.clear();}
+};
